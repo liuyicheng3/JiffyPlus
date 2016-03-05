a61dda2b238f7a8dff0abfe2c4b318a45fe4dcc8
lyc <liuyicheng3@gmail.com>
2016-03-05 17:25:05 +0800

    init

---
+++ app/src/main/java/com/veda/jiffyplus/JiffyActivity.java
@@ -0,0 +1,15 @@
+package com.veda.jiffyplus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lyc on 16-3-5.
+ */
+public class JiffyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
